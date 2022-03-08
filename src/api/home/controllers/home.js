e07f38c75eca5a85ddbbc42cda92b552a4c1815f
venkaCivsav <venka.civilizedsavage@gmail.com>
2022-03-08 15:19:58 +0530

    some chnages

--- src/api/home/controllers/home.js
+++ src/api/home/controllers/home.js
@@ -1,9 +1,55 @@
-'use strict';
+"use strict";
 
 /**
  *  home controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::home.home');
+module.exports = createCoreController("api::home.home");
+
+module.exports = createCoreController("api::home.home", ({ strapi }) => ({
+  async find(ctx) {
+    const { query } = ctx;
+
+    const entity = await strapi.entityService.findMany("api::home.home", {
+      ...query,
+      populate: {
+        hero: {
+          populate: {
+            heroImage: true,
+            listItems: true,
+          },
+        },
+        ourLearning: {
+          populate: {
+            activities: {
+              populate: {
+                image: true,
+              },
+            },
+          },
+        },
+        numberInfo: {
+          populate: {
+            image: true,
+          },
+        },
+        sections: {
+          populate: {
+            heroImage: true,
+            listItems: true,
+            activities: {
+              populate: {
+                image: true,
+              },
+            },
+          },
+        },
+      },
+    });
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(sanitizedEntity);
+  },
+}));
