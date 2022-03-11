bb323eb1c48919c9c9b5c504624fc3d5e1f06aae
venkaCivsav <venka.civilizedsavage@gmail.com>
2022-03-11 21:44:35 +0530

    chnages to component population in about singlr type

--- src/api/about/controllers/about.js
+++ src/api/about/controllers/about.js
@@ -7,3 +7,52 @@
 const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::about.about');
+
+
+module.exports = createCoreController("api::about.about", ({ strapi }) => ({
+    async find(ctx) {
+      const { query } = ctx;
+  
+      const entity = await strapi.entityService.findMany("api::about.about", {
+        ...query,
+        populate: {
+          banner: {
+            populate: {
+              heroImage: true,
+              listItems: true,
+            },
+          },
+         whyCards: {
+             populate:{
+                 listItems: true,
+             }
+         },
+         tautMoreFeatures:{
+             populate:{
+                 heroImage: true,
+                 listItems: true,
+             }
+         },
+         works:{
+             populate:{
+                 listItems: true,
+             }
+         },
+         carousel:{
+             populate:{
+                 carouselcard:{
+                     populate:{
+                         image: true
+                     }
+                 }
+             }
+         },
+         FAQ: true
+        },
+      });
+      const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+  
+      return this.transformResponse(sanitizedEntity);
+    },
+  }));
+  
