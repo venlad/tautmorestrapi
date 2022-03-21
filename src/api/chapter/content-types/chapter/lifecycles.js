32d815c5aa6e8723f0d737d7e97d408d8243c08d
venkaCivsav <venka.civilizedsavage@gmail.com>
2022-03-21 17:36:28 +0530

    slugify

--- src/api/chapter/content-types/chapter/lifecycles.js
+++ src/api/chapter/content-types/chapter/lifecycles.js
@@ -1,32 +1,9 @@
 const axios = require("axios");
-
-const STRAPI_URL = "http://localhost:1337";
-
-function string_to_slug(str) {
-  str = str.replace(/^\s+|\s+$/g, ""); // trim
-  str = str.toLowerCase();
-
-  // remove accents, swap ñ for n, etc
-  var from = "åàáãäâèéëêìíïîòóöôùúüûñç·/_,:;";
-  var to = "aaaaaaeeeeiiiioooouuuunc------";
-
-  for (var i = 0, l = from.length; i < l; i++) {
-    str = str.replace(new RegExp(from.charAt(i), "g"), to.charAt(i));
-  }
-
-  str = str
-    .replace(/[^a-z0-9 -]/g, "") // remove invalid chars
-    .replace(/\s+/g, "-") // collapse whitespace and replace by -
-    .replace(/-+/g, "-") // collapse dashes
-    .replace(/^-+/, "") // trim - from start of text
-    .replace(/-+$/, ""); // trim - from end of text
-
-  return str;
-}
+const slugify = require("slugify");
 
 module.exports = {
   async afterCreate(event) {
-    const { result, params } = event;
+    const { result } = event;
 
     // do something to the result;
     await axios
@@ -34,14 +11,12 @@ module.exports = {
         data: {
           topic: result.topic.map((item) => {
             return {
-              topicName: item?.topicName,
-              topicDescription: item?.topicDescription,
-              slug: string_to_slug(item.topicName),
+              ...item,
+              slug: item?.slug || slugify(item?.topicName, { lower: true }),
               subTopic: item.subTopic.map((sub) => {
                 return {
-                  subTopicName: sub?.subTopicName,
-                  subTopicDescription: sub?.subTopicDescription,
-                  slug: string_to_slug(sub.subTopicName),
+                  ...sub,
+                  slug: sub?.slug || slugify(sub.subTopicName, { lower: true }),
                 };
               }),
             };
@@ -55,7 +30,7 @@ module.exports = {
   },
 
   async afterUpdate(event) {
-    const { result, params } = event;
+    const { result } = event;
 
     // do something to the result;
     await axios
@@ -63,14 +38,12 @@ module.exports = {
         data: {
           topic: result.topic.map((item) => {
             return {
-              topicName: item?.topicName,
-              topicDescription: item?.topicDescription,
-              slug: item?.slug || string_to_slug(item.topicName),
+              ...item,
+              slug: item?.slug || slugify(item?.topicName, { lower: true }),
               subTopic: item.subTopic.map((sub) => {
                 return {
-                  subTopicName: sub?.subTopicName,
-                  subTopicDescription: sub?.subTopicDescription,
-                  slug: sub?.slug || string_to_slug(sub.subTopicName),
+                  ...sub,
+                  slug: sub?.slug || slugify(sub.subTopicName, { lower: true }),
                 };
               }),
             };
