1e630bb4c4e43df996fdf72e23d1a13f3c94711a
venkaCivsav <venka.civilizedsavage@gmail.com>
2022-03-25 22:37:53 +0530

    lifecycle URL change

--- config/database.js
+++ config/database.js
@@ -1,33 +1,33 @@
 const path = require('path');
 
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'sqlite',
-//     connection: {
-//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-//     },
-//     useNullAsDefault: true,
-//   },
-// });
-
-
-postgres: module.exports = ({ env }) => ({
+module.exports = ({ env }) => ({
   connection: {
-    client: "postgres",
+    client: 'sqlite',
     connection: {
-      host: "ec2-52-201-124-168.compute-1.amazonaws.com",
-      port: 5432,
-      database: "d716h515rqcb1r",
-      user: "ymsiuqqqkdadvd",
-      password:
-        "0774a87650635b40302d8c7ac8700a67a588c3fdc35dc06db659c62837bdd73f",
-      ssl: {
-        rejectUnauthorized: false,
-      },
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
-    debug: false,
+    useNullAsDefault: true,
   },
 });
+
+
+// postgres: module.exports = ({ env }) => ({
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: "ec2-52-201-124-168.compute-1.amazonaws.com",
+//       port: 5432,
+//       database: "d716h515rqcb1r",
+//       user: "ymsiuqqqkdadvd",
+//       password:
+//         "0774a87650635b40302d8c7ac8700a67a588c3fdc35dc06db659c62837bdd73f",
+//       ssl: {
+//         rejectUnauthorized: false,
+//       },
+//     },
+//     debug: false,
+//   },
+// });
 
 
 
