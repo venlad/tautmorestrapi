201d574cc9d737d7428ef386851954fac80ee172
venkaCivsav <venka.civilizedsavage@gmail.com>
2022-04-06 19:03:06 +0530

    code clean up

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require("path");
 
 // module.exports = ({ env }) => ({
 //   connection: {
@@ -9,7 +9,6 @@ const path = require('path');
 //     useNullAsDefault: true,
 //   },
 // });
-
 
 postgres: module.exports = ({ env }) => ({
   connection: {
@@ -29,5 +28,7 @@ postgres: module.exports = ({ env }) => ({
   },
 });
 
-// postgres://dkzgmcbcxwkvsq:9986f1b04301851d89626dfcf35afd9e3a8828ccd0ec44a1ef0973013c1f28eb@ec2-34-231-63-30.compute-1.amazonaws.com:5432/d86i7lopjg49s2
-// // postgres://vdxycadlzygxmz:fcaa6fdeef2803fb09a614cd5dba7e9003872a06c5e71c7fd744426ca59d4f63@ec2-18-214-134-226.compute-1.amazonaws.com:5432/d34pkmr7t8sucd
+// // postgres://dkzgmcbcxwkvsq:9986f1b04301851d89626dfcf35afd9e3a8828ccd0ec44a1ef0973013c1f28eb@ec2-34-231-63-30.compute-1.amazonaws.com:5432/d86i7lopjg49s2
+// // // postgres://vdxycadlzygxmz:fcaa6fdeef2803fb09a614cd5dba7e9003872a06c5e71c7fd744426ca59d4f63@ec2-18-214-134-226.compute-1.amazonaws.com:5432/d34pkmr7t8sucd
+
+// postgres://dthqgduemabmmw:43ed26d4f887fdb60c3f9de98d0cdbc3a2de8bb56749f7015b06fbe2982d33fb@ec2-52-30-67-143.eu-west-1.compute.amazonaws.com:5432/d22suhbtsg5hl9
