c0d15ce8a41ab306f854008726b4d5b7ed1bcda9
venkaCivsav <venka.civilizedsavage@gmail.com>
2022-04-07 18:15:10 +0530

    restriction and URL env

--- src/api/chapter/content-types/chapter/lifecycles.js
+++ src/api/chapter/content-types/chapter/lifecycles.js
@@ -3,63 +3,70 @@ const slugify = require("slugify");
 
 module.exports = {
   async afterCreate(event) {
+    console.log(event);
     const { result } = event;
 
     // do something to the result;
-    result.topic && await axios
-      .put(`https://tautmore.herokuapp.com/api/chapters/${result.id}`, {
-        data: {
-          topic: result.topic.map((item) => {
-            return {
-              ...item,
-              slug: item?.slug || slugify(item?.topicName, { lower: true }),
-              subTopic: item.subTopic.map((sub) => {
-                return {
-                  ...sub,
-                  slug: sub?.slug || slugify(sub.subTopicName, { lower: true }),
-                };
-              }),
-            };
-          }),
-        },
-      })
-      .then((res) => {
-        if (res.status === 200) console.log("success");
-      })
-      .catch((err) => console.log(err));
+    result.topic &&
+      (await axios
+        .put(`${process.env.URL}/api/chapters/${result.id}`, {
+          data: {
+            topic: result.topic.map((item) => {
+              return {
+                ...item,
+                slug: item?.slug || slugify(item?.topicName, { lower: true }),
+                subTopic: item.subTopic.map((sub) => {
+                  return {
+                    ...sub,
+                    slug:
+                      sub?.slug || slugify(sub.subTopicName, { lower: true }),
+                  };
+                }),
+              };
+            }),
+          },
+        })
+        .then((res) => {
+          if (res.status === 200) console.log("success");
+        })
+        .catch((err) => console.log(err)));
   },
 
   async afterUpdate(event) {
     const { result } = event;
     // do something to the result;
-    result.topic && await axios
-    .put(`https://tautmore.herokuapp.com/api/chapters/${result.id}`, {
-      data: {
-        topic: result.topic.map((item, i) => {
-          return {
-            ...item,
-            topicNumber: `${result?.chapterNumber}.${i+1}`,
-            slug: item?.slug || slugify(item?.topicName, { lower: true }),
-            subTopic: item.subTopic.map((sub,j) => {
+    result.topic &&
+      (await axios
+        .put(`${process.env.URL}/api/chapters/${result.id}`, {
+          data: {
+            topic: result.topic.map((item, i) => {
               return {
-                ...sub,
-                subTopicNumber: `${result?.chapterNumber}.${i+1}.${j+1}`,
-                slug: sub?.slug || slugify(sub.subTopicName, { lower: true }),
-                section: sub.section.map((val, k) => {
+                ...item,
+                topicNumber: `${result?.chapterNumber}.${i + 1}`,
+                slug: item?.slug || slugify(item?.topicName, { lower: true }),
+                subTopic: item.subTopic.map((sub, j) => {
                   return {
-                    ...val,
-                    serialNumber: `${k+1}`
-                  }
-                })
+                    ...sub,
+                    subTopicNumber: `${result?.chapterNumber}.${i + 1}.${
+                      j + 1
+                    }`,
+                    slug:
+                      sub?.slug || slugify(sub.subTopicName, { lower: true }),
+                    section: sub.section.map((val, k) => {
+                      return {
+                        ...val,
+                        serialNumber: `${k + 1}`,
+                      };
+                    }),
+                  };
+                }),
               };
             }),
-          };
-        }),
-      },
-    })
-    .then((res) => {
-      if (res.status === 200) console.log("success");
-    })
-    .catch((err) => console.log(err.message));
+          },
+        })
+        .then((res) => {
+          if (res.status === 200) console.log("success");
+        })
+        .catch((err) => console.log(err.message)));
   },
 };
