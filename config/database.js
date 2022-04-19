7a69017cb12998b7de442e9bf1d3d6091a4778e3
venkaCivsav <venka.civilizedsavage@gmail.com>
2022-04-19 13:23:07 +0530

    chnages in DB

--- config/database.js
+++ config/database.js
@@ -1,32 +1,32 @@
-const path = require("path");
+// const path = require("path");
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: "sqlite",
-    connection: {
-      filename: path.join(
-        __dirname,
-        "..",
-        env("DATABASE_FILENAME", ".tmp/data.db")
-      ),
-    },
-    useNullAsDefault: true,
-  },
-});
-
-// postgres: module.exports = ({ env }) => ({
+// module.exports = ({ env }) => ({
 //   connection: {
-//     client: "postgres",
+//     client: "sqlite",
 //     connection: {
-//       host: env("DATABASE_HOST", "127.0.0.1"),
-//       port: env.int("DATABASE_PORT", 5432),
-//       database: env("DATABASE_NAME", "strapi"),
-//       user: env("DATABASE_USERNAME", "strapi"),
-//       password: env("DATABASE_PASSWORD", "strapi"),
-//       ssl: {
-//         rejectUnauthorized: false,
-//       },
+//       filename: path.join(
+//         __dirname,
+//         "..",
+//         env("DATABASE_FILENAME", ".tmp/data.db")
+//       ),
 //     },
-//     debug: false,
+//     useNullAsDefault: true,
 //   },
 // });
+
+postgres: module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "strapi"),
+      password: env("DATABASE_PASSWORD", "strapi"),
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+    debug: false,
+  },
+});
