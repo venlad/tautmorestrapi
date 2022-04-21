15232c061e63df1b2a9dbaa968a6c18d730718bb
venkaCivsav <venka.civilizedsavage@gmail.com>
2022-04-21 22:02:35 +0530

    chnage of middleware

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,27 +1,24 @@
-// path: ./config/middlewares.js
-
 module.exports = [
-  'strapi::errors',
-    {
-      name: 'strapi::security',
-      config: {
-        contentSecurityPolicy: {
-          useDefaults: true,
-          directives: {
-            'connect-src': ["'self'", 'https:'],
-            'img-src': ["'self'", 'data:', 'blob:', 'stockydodo-web-admin-images.s3.ap-south-1.amazonaws.com'],
-            'media-src': ["'self'", 'data:', 'blob:', 'stockydodo-web-admin-images.s3.ap-south-1.amazonaws.com'],
-            upgradeInsecureRequests: null,
-          },
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          upgradeInsecureRequests: null,
         },
       },
     },
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::favicon",
+  "strapi::public",
 ];
