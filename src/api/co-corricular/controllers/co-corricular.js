dc8d70a04510bc75e169bff00ad173de489cd32d
venkaCivsav <venka.civilizedsavage@gmail.com>
2022-04-25 16:14:43 +0530

    chnages to collection type

--- src/api/co-corricular/controllers/co-corricular.js
+++ src/api/co-corricular/controllers/co-corricular.js
@@ -6,4 +6,61 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::co-corricular.co-corricular');
+// module.exports = createCoreController('api::co-corricular.co-corricular');
+
+module.exports = createCoreController("api::co-corricular.co-corricular", ({ strapi }) => ({
+    async find(ctx) {
+      const { query } = ctx;
+  
+      const entity = await strapi.entityService.findMany("api::co-corricular.co-corricular", {
+        ...query,
+        populate: {
+          icon: true,
+          chapters: {
+            populate: {
+              topic: {
+                populate: {
+                  subTopic: {
+                    populate:{
+                      section: true,
+                  videoAndMedia: true
+                    }
+                  },
+                },
+              },
+              grade: true,
+              subject: true
+            },
+          },
+          activity: {
+            populate: {
+              subjects: {
+                populate: {
+                  icon: true,
+                  chapters: {
+                    populate: {
+                      topic: {
+                        populate: {
+                          subTopic: {
+                            populate:{
+                              section: true,
+                  videoAndMedia: true
+                            }
+                          },
+                        },
+                      },
+                      grade: true,
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      });
+      const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+  
+      return this.transformResponse(sanitizedEntity);
+    },
+  }));
+  
